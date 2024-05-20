fc9f69374f2d289678a2ab361b204c1ece506860
JShin720 <jeffshin720@gmail.com>
2024-05-20 13:43:01 -0400

    making form in the signup component

--- src/component/signup/Signup.jsx
+++ src/component/signup/Signup.jsx
@@ -3,15 +3,18 @@ import React from 'react'
 export default function Signup() {
   return (
     <>
-    <div className="step-info"></div>
-    <h1>Personal Info</h1>
-    <h4>Please provide your name, email address, and phone number.</h4>
-    <form action="">
-        <input type="text" name="name" id="name" /> 
+      <div className="step-info"></div>
+      <h1>Personal Info</h1>
+      <h4>Please provide your name, email address, and phone number.</h4>
+      <form action="">
+        <label for="name"></label>
+        <input type="text" name="name" id="name" />
+        <label for="email"></label>
         <input type="email" name="email" id="email" />
+        <label for="phone-number"></label>
         <input type="tel" name="phone-number" id="phone-number" />
         <input type="submit" value="" />
-    </form>
+      </form>
     </>
-  )
+  );
 }
