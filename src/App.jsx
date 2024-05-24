87869323afc8516cd4e5f56bbfbe02ba60ceb774
JShin720 <jeffshin720@gmail.com>
2024-05-24 11:26:56 -0400

    making form in the signup component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
-import Signup from "./component/signup/Signup"
+import Signup from "./component/signup/Signup.jsx"
+import Login from "./component/login/Login.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
+import { ProtectedRoute } from "./component/ProtectedRoute.jsx";
 import './App.css'
 
 function App() {
