1e5860b99d64f1c527f61ac3f3d10e49c14045c3
JShin720 <jeffshin720@gmail.com>
2024-05-28 15:35:33 -0400

    making form in the signup component

---
+++ src/component/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Form() {
+  return (
+    <div className='form'>
+      <div className="progress-bar">
+        <div className="form-container">
+            
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Form;
