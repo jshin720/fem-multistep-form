67b7584a76923f5ec2b188599629632661c12918
JShin720 <jeffshin720@gmail.com>
2024-05-28 15:42:06 -0400

    making form in the signup component

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -4,13 +4,16 @@ function Form() {
     const [page, setPage] = useState(0);
     const formTitles = ["signup", "personalinfo", "other"];
 
-    
+
     
   return (
     <div className="form">
       <div className="progress-bar">
         <div className="form-container">
-          <div className="header"></div>
+          <div className="header">
+            <h1>{formTitles[page]}</h1>
+
+          </div>
           <div className="body"></div>
           <div className="footer">
             <button></button>
