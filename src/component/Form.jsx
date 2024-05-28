83b979b161f5cee216f0a889397ed1afe8abdeda
JShin720 <jeffshin720@gmail.com>
2024-05-28 15:36:39 -0400

    making form in the signup component

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
 
 function Form() {
   return (
-    <div className='form'>
+    <div className="form">
       <div className="progress-bar">
         <div className="form-container">
-            
+          <div className="header"></div>
+          <div className="body"></div>
+          <div className="footer">
+            <button></button>
+            <button></button>
+          </div>
         </div>
       </div>
     </div>
