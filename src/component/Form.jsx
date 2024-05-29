f3fba52f4076df4be63e74e9f9bfa5dcfd5cdb54
JShin720 <jeffshin720@gmail.com>
2024-05-29 14:37:21 -0400

    making form in the signup component

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,25 +1,27 @@
 import React, { useState } from "react";
 
 function Form() {
-    const [page, setPage] = useState(0);
-    const formTitles = ["signup", "personalinfo", "other"];
+  const [page, setPage] = useState(0);
+  const formTitles = ["signup", "personalinfo", "other"];
 
-
-    
   return (
     <div className="form">
       <div className="progress-bar">
         <div className="form-container">
           <div className="header">
             <h1>{formTitles[page]}</h1>
-
           </div>
           <div className="body"></div>
-          <div className="footer"> 
-            <button onClick={() => {setPage(currPage) => currPage + 1 }>next</button>
-            <button>
-                prev
+          <div className="footer">
+            <button
+              onClick={() => {
+                setPage((currPage) => currPage + 1);
+              }}
+            >
+              {" "}
+              next
             </button>
+            <button>prev</button>
           </div>
         </div>
       </div>
