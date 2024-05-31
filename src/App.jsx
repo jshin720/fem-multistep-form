2c58bb9da7ba3517e6a2ecb0bfe1d9a038969ebe
JShin720 <jeffshin720@gmail.com>
2024-05-31 12:18:03 -0400

    creating a state for the data

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
     <div className="progress-bar">
       <div className="progress" style={{ width: page === 0 ? "25%" : page === 1 ? "50%" : page === 2 ? "75%" : "100%" }}></div>
     </div>
-      <Form />
+      <Form formData={formData} setFormData={setFormData}/>
     </>
   );
 }
