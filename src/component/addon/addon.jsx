dd9f3aea6ab5c5ede8f384a5e8adeb54be732a3e
JShin720 <jeffshin720@gmail.com>
2024-06-07 15:00:54 -0400

    creating form for the personal info component

--- src/component/addon/addon.jsx
+++ src/component/addon/addon.jsx
@@ -4,7 +4,14 @@ function addOn() {
   return (
     <div>
       <h1>addon</h1>
-        
+      <form>
+        <label>Username:</label>
+        <input type="text" name="name" value={handleChange()} />
+        <label>Email:</label>
+        <input type="email" name="email" value={handleChange()} />
+        <label>Phone Number</label>
+        <input type="phone" name="phone" value={handleChange()} />
+      </form>
     </div>
   );
 }
