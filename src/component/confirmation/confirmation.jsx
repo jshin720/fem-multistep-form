65be12bc9445952891570a0fd2d7e5bc43f5b99b
JShin720 <jeffshin720@gmail.com>
2024-06-07 15:01:09 -0400

    creating form for the personal info component

--- src/component/confirmation/confirmation.jsx
+++ src/component/confirmation/confirmation.jsx
@@ -4,6 +4,14 @@ function confirmation() {
   return (
     <div>
       <h1>confirmation</h1>
+      <form>
+        <label>Username:</label>
+        <input type="text" name="name" value={handleChange()} />
+        <label>Email:</label>
+        <input type="email" name="email" value={handleChange()} />
+        <label>Phone Number</label>
+        <input type="phone" name="phone" value={handleChange()} />
+      </form>
     </div>
   );
 }
