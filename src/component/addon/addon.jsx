08deaa7dfd6575712b756753325b6de249222cb2
JShin720 <jeffshin720@gmail.com>
2024-06-12 16:07:07 -0400

    creating a toggleswitch

--- src/component/addon/addon.jsx
+++ src/component/addon/addon.jsx
@@ -5,17 +5,26 @@ function addOn() {
     <div>
       <h1>Pick addon</h1>
       <h5>add-ons help enhance your gaming experience</h5>
-     <div>
-      <input type="checkbox" name="" id="" />
-      <span>
-        <h5>
-          online services
-        </h5>
-        <p>
-          access to multiplayer games
-        </p>
-      </span>
-     </div>
+      <div>
+        <input type="checkbox" name="" id="" />
+        <span>
+          <h5>online services</h5>
+          <p>access to multiplayer games</p>
+        </span>
+      </div>
+      <div>
+        <p>$1/mo</p>
+      </div>
+      <div>
+        <input type="checkbox" name="" id="" />
+        <span>
+          <h5>online services</h5>
+          <p>access to multiplayer games</p>
+        </span>
+      </div>
+      <div>
+        <p>$1/mo</p>
+      </div>
     </div>
   );
 }
