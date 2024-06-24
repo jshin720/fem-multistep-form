c0b44f43ec23d25d72a238786c196efa7b20bb86
JShin720 <jeffshin720@gmail.com>
2024-06-24 14:40:53 -0400

    creating summary component

--- src/component/summary/summary.jsx
+++ src/component/summary/summary.jsx
@@ -1,14 +1,34 @@
 import React, {useState} from 'react';
 
 function summary(props) {
-  const [summary, setSummary] = useState() 
+  const [confirmed, setConfirmed] = useState(false) 
+
+  const handleConfirmed = () => {
+    
+  }
 
   return (
     <div>
       <h1>Finshing up</h1>
       <p>Double-check everything looks ok before confirming</p>
+      <span>
+        <li>
+          <ul>
 
-      
+          </ul>
+          <ul>
+
+          </ul>
+          <ul>
+
+          </ul>
+          <ul>
+            Total
+          </ul>
+        </li>
+      </span>
+
+      <button onClick={setConfirmed(true)}></button>
     </div>
   );
 }
