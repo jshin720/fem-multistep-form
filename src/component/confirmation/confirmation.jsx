2f526003df593ab03cf0e9a4659f66912922cbba
JShin720 <jeffshin720@gmail.com>
2024-07-03 13:08:33 -0400

    working on forms

--- src/component/confirmation/confirmation.jsx
+++ src/component/confirmation/confirmation.jsx
@@ -5,7 +5,11 @@ function confirmation(props) {
   const { formData, setFormData, page, setPage } = props;
 
   const handleChange = () => {
-    
+      setFormData({
+        ...formData,
+        page: page + 1
+        })
+        }
   }
 
 
