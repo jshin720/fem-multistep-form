fecec124c625b47caba68fdc71e7d9d277cab8bb
JShin720 <jeffshin720@gmail.com>
2024-07-09 14:49:08 -0400

    re adding buttons on addon component

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -31,7 +31,7 @@ function Form(props) {
   console.log(props);
   const { page, setPage } = props;
   
-
+  const { name, email, phone, termToggle, monthly, yearly, addOns }
   
 
   const pageDisplay = () => {
